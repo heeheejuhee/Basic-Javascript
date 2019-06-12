441a0842e393f0d5ae00cc406bbf31e04af6d1b6
Juhee Han <200859841@cit3-501m-08.local>
2019-06-12 14:30:00 +1200

    Lush website

--- Lush website/assets/script.js
+++ Lush website/assets/script.js
@@ -1,13 +1,32 @@
 $(function(){
 
     var anime1 = anime({
-        targets: '.section2 .layer',
+        targets: '.section2 .layer0',
+        translateY: function(layer,i,l){
+            return (l-i)*50;
+        },
+        easing: 'linear',
+        duration: 8000,
+        autoplay: false,
+    });
+    var anime2 = anime({
+        targets: '.section2 .layer1',
         translateY: function(layer,i,l){
             return (l-i)*50;
 
         },
         easing: 'linear',
-        duration: 6000,
+        duration: 3000,
+        autoplay: false,
+    });
+    var anime3 = anime({
+        targets: '.section2 .layer2',
+        translateY: function(layer,i,l){
+            return (l-i)*50;
+
+        },
+        easing: 'linear',
+        duration: 1000,
         autoplay: false,
     });
 
@@ -19,6 +38,9 @@ $(function(){
         var progress = scrollTop/scrollLength;
 
         anime1.seek(anime1.duration*progress);
+        anime2.seek(anime2.duration*progress);
+        anime3.seek(anime3.duration*progress);
+
 
     });
     
